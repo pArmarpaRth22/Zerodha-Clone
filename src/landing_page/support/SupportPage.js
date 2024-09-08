f2de1e4ef425770ffada3556b05fdad534518c7d
pArmarpaRth22 <parmarparth10945@gmail.com>
2024-09-08 16:00:19 +0530

    home page without footer

---
+++ src/landing_page/support/SupportPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Hero from "./Hero";
+import CreateTickit from "./CreateTickit";
+import Footer from "../Footer";
+
+function SupportPage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <CreateTickit />
+      <Footer />
+    </>
+  );
+}
+
+export default SupportPage;
